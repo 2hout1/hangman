9de402f07211e90f2cebd48885355162a79c8f0c
2hout1 <34408339+2hout1@users.noreply.github.com>
2017-12-10 20:44:24 +0800

    Initial commit

---
+++ TwoPlayerHangman.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hangman;
+
+/**
+ *
+ * @author Tina ZHOU
+ */
+import java.util.*;
+import java.io.*;
+
+public class TwoPlayerHangman implements Hangmanprocess {
+//public class TwoPlayerHangman {
+  private static int MAX_LENGTH = 8;
+  private static int MAX_INCORRECT_GUESSES = 10;
+  private static int word_length;
+  private static int numberOfTries;
+  private static String wordToGuess;
+  private static boolean wordIsGuessed;
+  private static String playerOneName;
+
+  public void process() {
+
+    Scanner scan = new Scanner (System.in);
+    char guessedCharacter;
+
+    System.out.println("Hello! Welcome to the multiplayer section of Hangman, where you learn new words with your friends!");
+    System.out.println("This game is only suitable for two players (Player One and Player Two), and Player One goes first.");
+    System.out.println("Player One, Please enter your name.");
+    playerOneName = scan.nextLine();
+    System.out.println("Welcome, " + playerOneName + "! Please enter the word you wish to be guessed.");
+
+    wordToGuess = scan.next(); // Player One inputs the word
+    word_length = wordToGuess.length(); // The game retrieves the length of the word and puts each letter into an array
+    System.out.println("Please enter the number of guesses you will allow Player 2 to have. (minimum of " + word_length + " guesses!)");
+    numberOfTries = scan.nextInt(); // Player One inputs the number of guesses allowed.
+
+    while (numberOfTries == 0 || numberOfTries < word_length) {
+        System.out.println("Sorry! You must give a minimum of " + word_length + " guesses!");
+        numberOfTries = scan.nextInt();
+    }
+
+    System.out.print('\u000C');
+
+    // This part is for Player 2.
+
+    int numberGuessedFalse = 0; // Number of incorrect letter guesses
+    int numberGuessedTrue = 0; // Number of correct letter guesses
+    char[] revealedLetters = new char[word_length];// convert to char array
+
+    for (int rli = 0; rli<revealedLetters.length; rli++) //revealed letter index
+      revealedLetters[rli] = '*';
+
+    System.out.println("Player Two, guess your letters! (" + numberOfTries + " wrong guesses, then you lose!)");
+    boolean wordIsGuessed = false;
+    //String originalWord = new String(revealedLetters); // converts the char array revealedLetters to String type to make use of .contains method
+
+    // Main code starts here
+
+
+    
+    while((new String(revealedLetters)).contains("*") && numberGuessedFalse<numberOfTries){      // while x stills contains _
+
+      // fill logic
+      System.out.println("(Guess) Enter a letter in word ");
+      Scanner input = new Scanner(System.in);
+      char userInput = input.nextLine().charAt(0);
+      int matchedCharAt = wordToGuess.indexOf(userInput);
+      if (matchedCharAt != -1) {
+        revealedLetters[matchedCharAt] = userInput;
+        while (matchedCharAt != -1) {
+          matchedCharAt = wordToGuess.indexOf(userInput, matchedCharAt+1);
+          if (matchedCharAt != -1) {
+            revealedLetters[matchedCharAt] = userInput;
+          }
+        }
+        System.out.print(" > ");
+        System.out.println(new String(revealedLetters));
+      }else{
+        numberGuessedFalse++;
+        System.out.println("Oops!" + numberGuessedFalse + " of " + numberOfTries + " wrong guesses: " + new String(revealedLetters));
+      }
+    }
+
+    if (!new String(revealedLetters).contains("*")){
+      System.out.println("Congratulations! You got: " + wordToGuess);
+    }else{
+      System.out.println("Better luck next time. The word was: " + wordToGuess);
+    }
+
+  }
+}
